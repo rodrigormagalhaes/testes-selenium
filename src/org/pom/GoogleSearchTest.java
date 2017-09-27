fb1737dde6e9be1906dd6a28e9282a43748612a7
Rodrigo Magalhães <rodrigorm.rm@gmail.com>
2017-09-27 20:55:39 -0300

    Criação do projeto

---
+++ src/org/pom/GoogleSearchTest.java
@@ -0,0 +1,34 @@
+package org.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class GoogleSearchTest {
+	
+	public static void main (String[] args) {
+		
+		//System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+		
+		//WebDriver driver = new FirefoxDriver();
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		
+		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
+				
+		WebDriver driver = new ChromeDriver(options);
+		
+		driver.navigate().to("http://www.google.com");
+		
+		GoogleHomePageObjects page = new GoogleHomePageObjects(driver);
+		
+		page.txtSearch.sendKeys("Rodrigo Magalhães");
+		page.btnSearch.click();
+		
+		driver.close();
+	}
+	
+}
+
+
